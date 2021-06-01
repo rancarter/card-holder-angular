66d33a7d223140aaebff944d0d87a79c776b3fcf
Vitalii Korchak <vitalii.korchak@gmail.com>
2021-06-01 18:48:34 +0300

    add CardListComponent

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,9 +4,10 @@ import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from "./app.component";
 import { CardFormComponent } from './card-form/card-form.component';
+import { CardListComponent } from './card-list/card-list.component';
 
 @NgModule({
-  declarations: [AppComponent, CardFormComponent],
+  declarations: [AppComponent, CardFormComponent, CardListComponent],
   imports: [BrowserModule, FormsModule],
   providers: [],
   bootstrap: [AppComponent],
---
+++ src/app/card-list/card-list.component.ts
@@ -0,0 +1,12 @@
+import { Component, Input } from '@angular/core';
+
+import { Card } from '../cards.service';
+
+@Component({
+  selector: 'app-card-list',
+  templateUrl: './card-list.component.html',
+  styleUrls: ['./card-list.component.scss'],
+})
+export class CardListComponent {
+  @Input() cards: Card[] = [];
+}
